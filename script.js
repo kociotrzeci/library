5d4f231b2de50a689317c4129cb8cc9513ca6999
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-01-07 00:42:05 +0100

    adding, removing and marking read

--- script.js
+++ script.js
@@ -6,24 +6,70 @@ const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
 const table = document.querySelector("tbody");
 
-
-function Book(author, title, pages, read) {
+class Book {
+  static id = -1;
+  constructor(author, title, pages, read){
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
-}
-
-function reload(){
-  let row;
-  while (table.firstChild){
-    table.removeChild(table.lastChild)
+  Book.id++;
+  this.id = Book.id;
   }
 }
 
+myLibrary.push(new Book(
+  "Sienkiewicz Henryk",
+  "Ogniem i Mieczem",
+  "432",
+  true,
+));
 
 
 
+function remove(id){
+  let remID = myLibrary.map(function(e){
+    return e.id;
+  }).indexOf(id);
+  myLibrary.splice(remID, 1);
+  reload();
+};
+
+function reload(){
+  let i = 0;
+  while (table.firstChild){
+    table.removeChild(table.firstChild)
+  }
+  myLibrary.forEach((book =>{
+    const tableRow = document.createElement("tr");
+    const author = document.createElement("td");
+    const title = document.createElement("td");
+    const pages = document.createElement("td");
+    const read = document.createElement("td");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = book.read;
+    checkbox.addEventListener(("change"), function(event){
+      book.read = !book.read;
+    })
+    const removeButton = document.createElement("button");
+    removeButton.className = "removeButton";
+    removeButton.textContent = "X";
+    removeButton.addEventListener(("click"), function(event){
+      remove(book.id);
+    })
+    read.appendChild(checkbox);
+    author.textContent = book.author;
+    title.textContent = book.title;
+    pages.textContent = book.pages;
+    tableRow.appendChild(author);
+    tableRow.appendChild(title);
+    tableRow.appendChild(pages);
+    tableRow.appendChild(read); 
+    tableRow.appendChild(removeButton);
+    table.appendChild(tableRow);
+  }))
+}
 
 
 // "Show the dialog" button opens the dialog modally
@@ -45,5 +91,4 @@ closeButton.addEventListener("click", function(event) {
   dialog.close();
 });
 
-
-
+reload();
