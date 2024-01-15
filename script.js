5a92857c43210fdb5903f8ecd039a808ba7b81cf
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-01-15 16:17:04 +0100

    test stge1

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const table = document.querySelector("tbody");
 
 class Book {
   static id = -1;
-  constructor(author, title, pages, read){
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-  Book.id++;
-  this.id = Book.id;
+  constructor(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    Book.id++;
+    this.id = Book.id;
   }
 }
 
@@ -24,23 +24,19 @@ myLibrary.push(new Book(
   "432",
   true,
 ));
-
-
-
-function remove(id){
-  let remID = myLibrary.map(function(e){
+function remove(id) {
+  let remID = myLibrary.map(function (e) {
     return e.id;
   }).indexOf(id);
   myLibrary.splice(remID, 1);
   reload();
 };
-
-function reload(){
+function reload() {
   let i = 0;
-  while (table.firstChild){
+  while (table.firstChild) {
     table.removeChild(table.firstChild)
   }
-  myLibrary.forEach((book =>{
+  myLibrary.forEach((book => {
     const tableRow = document.createElement("tr");
     const author = document.createElement("td");
     const title = document.createElement("td");
@@ -49,13 +45,13 @@ function reload(){
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.checked = book.read;
-    checkbox.addEventListener(("change"), function(event){
+    checkbox.addEventListener(("change"), function (event) {
       book.read = !book.read;
     })
     const removeButton = document.createElement("button");
     removeButton.className = "removeButton";
     removeButton.textContent = "X";
-    removeButton.addEventListener(("click"), function(event){
+    removeButton.addEventListener(("click"), function (event) {
       remove(book.id);
     })
     read.appendChild(checkbox);
@@ -65,20 +61,18 @@ function reload(){
     tableRow.appendChild(author);
     tableRow.appendChild(title);
     tableRow.appendChild(pages);
-    tableRow.appendChild(read); 
+    tableRow.appendChild(read);
     tableRow.appendChild(removeButton);
     table.appendChild(tableRow);
   }))
 }
-
-
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
 // "Close" button closes the dialog
-closeButton.addEventListener("click", function(event) {
+closeButton.addEventListener("click", function (event) {
   event.preventDefault();
   myLibrary.push(new Book(
     form.elements["author"].value,
@@ -90,5 +84,6 @@ closeButton.addEventListener("click", function(event) {
   reload();
   dialog.close();
 });
-
+//test1
+//test2
 reload();
