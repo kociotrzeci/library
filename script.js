80164a9f8d551e2c81af402974bac02819a7adb3
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-03-04 22:18:54 +0100

    added form validation

--- script.js
+++ script.js
@@ -18,25 +18,22 @@ class Book {
   }
 }
 
-myLibrary.push(new Book(
-  "Sienkiewicz Henryk",
-  "Ogniem i Mieczem",
-  "432",
-  true,
-));
+myLibrary.push(new Book("Sienkiewicz Henryk", "Ogniem i Mieczem", "432", true));
 function remove(id) {
-  let remID = myLibrary.map(function (e) {
-    return e.id;
-  }).indexOf(id);
+  let remID = myLibrary
+    .map(function (e) {
+      return e.id;
+    })
+    .indexOf(id);
   myLibrary.splice(remID, 1);
   reload();
-};
+}
 function reload() {
   let i = 0;
   while (table.firstChild) {
-    table.removeChild(table.firstChild)
+    table.removeChild(table.firstChild);
   }
-  myLibrary.forEach((book => {
+  myLibrary.forEach((book) => {
     const tableRow = document.createElement("tr");
     const author = document.createElement("td");
     const title = document.createElement("td");
@@ -45,15 +42,15 @@ function reload() {
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.checked = book.read;
-    checkbox.addEventListener(("change"), function (event) {
+    checkbox.addEventListener("change", function (event) {
       book.read = !book.read;
-    })
+    });
     const removeButton = document.createElement("button");
     removeButton.className = "removeButton";
     removeButton.textContent = "X";
-    removeButton.addEventListener(("click"), function (event) {
+    removeButton.addEventListener("click", function (event) {
       remove(book.id);
-    })
+    });
     read.appendChild(checkbox);
     author.textContent = book.author;
     title.textContent = book.title;
@@ -64,26 +61,28 @@ function reload() {
     tableRow.appendChild(read);
     tableRow.appendChild(removeButton);
     table.appendChild(tableRow);
-  }))
+  });
 }
-// "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// "Close" button closes the dialog
 closeButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  myLibrary.push(new Book(
-    form.elements["author"].value,
-    form.elements["title"].value,
-    form.elements["pages"].value,
-    form.elements["read"].checked,
-  ));
-  form.reset();
-  reload();
-  dialog.close();
+  if (form.checkValidity()) {
+    myLibrary.push(
+      new Book(
+        form.elements["author"].value,
+        form.elements["title"].value,
+        form.elements["pages"].value,
+        form.elements["read"].checked
+      )
+    );
+    form.reset();
+    reload();
+    dialog.close();
+  }
 });
-//test1
-//test2
+
 reload();
+dialog.showModal();
+//form-validation
